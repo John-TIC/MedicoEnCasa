64e60aeb79c5aa2642436a529ad5dac24b1c44f7
Carlos Arroyo <carloscha82@hotmail.com>
2022-07-11 23:55:01 -0500

    Se actualian templates HospiEnCasa.App.Presentacion

---
+++ HospiEnCasa.App.Presentacion/Pages/Pacientes/NuevoPaciente.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HospiEnCasa.App.Presentacion.Pages
+{
+    public class NuevoPacienteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
