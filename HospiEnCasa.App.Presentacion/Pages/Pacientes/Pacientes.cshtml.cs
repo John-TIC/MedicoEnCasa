9b5c141880f1e9805e609dd4efe8e80fb152a467
Michell Mendoza <mendoza.anghelo07@gmail.com>
2022-07-14 13:30:38 -0500

    consulta de pacientes en Pacientes.cshtml y cs

--- HospiEnCasa.App.Presentacion/Pages/Pacientes/Pacientes.cshtml.cs
+++ HospiEnCasa.App.Presentacion/Pages/Pacientes/Pacientes.cshtml.cs
@@ -4,13 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using HospiEnCasa.App.Persistencia;
+using HospiEnCasa.App.Dominio;
 
 namespace HospiEnCasa.App.Presentacion.Pages
 {
     public class PacientesModel : PageModel
     {
+        private readonly IRepositorioPaciente repositorioPaciente;
+        [BindProperty]
+        public IEnumerable<Paciente> Paciente {get; set;}
+        
+        public PacientesModel(IRepositorioPaciente repositorioPaciente)
+        {
+                this.repositorioPaciente = repositorioPaciente;
+        }
         public void OnGet()
         {
+            Paciente = repositorioPaciente.GetAllPacientes();
         }
     }
 }
