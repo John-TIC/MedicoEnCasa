499f161ae1fc97ccd662933e382f1e03ff438475
Santiago Acevedo <santiago.acevedo.m24@gmail.com>
2022-07-15 14:53:25 -0500

    Validación de registro de Paciente

--- HospiEnCasa.App.Dominio/Entidades/Paciente.cs
+++ HospiEnCasa.App.Dominio/Entidades/Paciente.cs
@@ -1,14 +1,20 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace HospiEnCasa.App.Dominio
 {
     public class Paciente : Persona
     {
+        [Required, StringLength(20)]
         public string Direccion { get; set; }
+        [Required, StringLength(15)]
         public float Latitud { get; set; }
+        [Required, StringLength(15)]
         public float Longitud { get; set; }
+        [Required, StringLength(20)]
         public string Ciudad { get; set; }
+        [Required, DataType(DataType.DateTime)]
         public DateTime FechaNacimiento { get; set; }
         public FamiliarDesignado FamiliarDesignado { get; set; }
         public Enfermera Enfermera { get; set; }
--- HospiEnCasa.App.Dominio/Entidades/Persona.cs
+++ HospiEnCasa.App.Dominio/Entidades/Persona.cs
@@ -1,13 +1,20 @@
 
+using System.ComponentModel.DataAnnotations;
+
 namespace HospiEnCasa.App.Dominio
 {
     public class Persona
     {
         public int Id { get; set; }
+        [Required, StringLength(10)]
         public string Identificacion {get; set;}
+        [Required, StringLength(30)]
         public string Nombres { get; set; }
+        [Required, StringLength(30)]
         public string Apellidos { get; set; }
+        [Required, StringLength(10)]
         public string NumeroTelefono { get; set; }
+        [Required]
         public Genero Genero { get; set; }
     }
 }
