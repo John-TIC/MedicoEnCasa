4728c6381ef0ba6e2657c3f4786b455f1790e548
Michell Mendoza <mendoza.anghelo07@gmail.com>
2022-07-16 20:03:50 -0500

    lista de Enfermeras

--- HospiEnCasa.App.Presentacion/Pages/Enfermeras/Enfermeras.cshtml.cs
+++ HospiEnCasa.App.Presentacion/Pages/Enfermeras/Enfermeras.cshtml.cs
@@ -4,13 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using HospiEnCasa.App.Persistencia;
+using HospiEnCasa.App.Dominio;
 
 namespace HospiEnCasa.App.Presentacion.Pages
 {
     public class EnfermerasModel : PageModel
     {
-        public void OnGet()
+        private readonly IRepositorioEnfermera repositorioEnfermera;
+        public IEnumerable<Enfermera> Enfermeras { get; set; }
+
+        public EnfermerasModel(IRepositorioEnfermera repositorioEnfermera)
         {
+            this.repositorioEnfermera = repositorioEnfermera;
+        }
+        public IActionResult OnGet()
+        {
+            Enfermeras = repositorioEnfermera.GetAllEnfermera();
+
+            if (Enfermeras == null)
+                return RedirectToPage("Error");
+            return Page();
+
         }
     }
 }
