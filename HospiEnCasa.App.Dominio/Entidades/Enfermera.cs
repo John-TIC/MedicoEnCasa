f2b4e0dda5ac207c1185f97df1ab53408e0ccc81
Carlos Arroyo <carloscha82@hotmail.com>
2022-07-17 17:35:26 -0500

    Se actualizan validaciones en algunas entidades del Dominio

--- HospiEnCasa.App.Dominio/Entidades/Enfermera.cs
+++ HospiEnCasa.App.Dominio/Entidades/Enfermera.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace HospiEnCasa.App.Dominio
 {
     public class Enfermera : Persona{
         
+        [Required(ErrorMessage = "Ingrese una Tarjeta Profesional")]
+        [StringLength(20, MinimumLength = 5, ErrorMessage = "Tarjeta Profesional debe tener entre 5 y 20 caracteres")]
         public string TarjetaProfesional {get; set;}
+        
         public int HorasLaborales {get; set;}
     }
 }
--- HospiEnCasa.App.Dominio/Entidades/Medico.cs
+++ HospiEnCasa.App.Dominio/Entidades/Medico.cs
@@ -1,9 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace HospiEnCasa.App.Dominio
 {
     public class Medico : Persona
     {
+        [Required(ErrorMessage = "Ingrese una Especialidad")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Especialidad debe tener entre 3 y 50 caracteres")]
         public string Especialidad {get; set;}
+
+        [Required(ErrorMessage = "Ingrese un Codigo")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Codigo debe tener entre 3 y 20 caracteres")]
         public string Codigo {get; set;}
+
+        [Required(ErrorMessage = "Ingrese un RegistroRethus")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "RegistroRethus debe tener entre 3 y 20 caracteres")]
         public string RegistroRethus {get; set;}
   
     }
