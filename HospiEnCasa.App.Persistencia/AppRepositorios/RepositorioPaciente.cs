e804b647e694f2fb610a473195dc1f7b9678f772
Michell Mendoza <mendoza.anghelo07@gmail.com>
2022-07-19 20:20:17 -0500

    update de telefono y direccion

--- HospiEnCasa.App.Persistencia/AppRepositorios/RepositorioPaciente.cs
+++ HospiEnCasa.App.Persistencia/AppRepositorios/RepositorioPaciente.cs
@@ -92,7 +92,7 @@ namespace HospiEnCasa.App.Persistencia
             var pacienteEncontrado = _appContext.Pacientes.FirstOrDefault(p => p.Id == paciente.Id);
             if (pacienteEncontrado != null)
             {
-                pacienteEncontrado.Identificacion = paciente.Identificacion;
+              pacienteEncontrado.Identificacion = paciente.Identificacion;
                 pacienteEncontrado.Nombres = paciente.Nombres;
                 pacienteEncontrado.Apellidos = paciente.Apellidos;
                 pacienteEncontrado.NumeroTelefono = paciente.NumeroTelefono;
--- HospiEnCasa.App.Presentacion/Pages/Pacientes/Paciente.cshtml.cs
+++ HospiEnCasa.App.Presentacion/Pages/Pacientes/Paciente.cshtml.cs
@@ -7,17 +7,21 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 using HospiEnCasa.App.Dominio;
 using HospiEnCasa.App.Persistencia;
+using HospiEnCasa.App.Presentacion.Model;
 
 namespace HospiEnCasa.App.Presentacion.Pages
 {
     public class PacienteModel : PageModel
     {
         [BindProperty]
-        public Paciente Paciente {get; set;}
+        public Paciente Paciente { get; set; }
+        public bool IsUpdatePaciente { get; set; }
+        public ModalInfo ModalInfo { get; set; }
 
         private readonly IRepositorioPaciente repositorioPaciente;
 
-        public PacienteModel(IRepositorioPaciente repositorioPaciente){
+        public PacienteModel(IRepositorioPaciente repositorioPaciente)
+        {
             this.repositorioPaciente = repositorioPaciente;
         }
 
@@ -25,12 +29,42 @@ namespace HospiEnCasa.App.Presentacion.Pages
         {
             Paciente = repositorioPaciente.GetPaciente(id);
 
-            if(Paciente == null)
+            if (Paciente == null)
                 return RedirectToPage("Error");
 
             ViewData["idPaciente"] = id;
 
             return Page();
         }
+        //Actualizar Paciente
+        public IActionResult OnPost()
+
+        {
+            IsUpdatePaciente = false;
+
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            if (Paciente.Id > 0)
+            {
+                Paciente = repositorioPaciente.UpdatePaciente(Paciente);
+                if (Paciente == null)
+                    return RedirectToPage("Error");
+
+                ModalInfo = new ModalInfo
+                {
+                    TitleModal = " Paciente Actualizado",
+                    MsgModal = "Paciente " + Paciente.Nombres + " " + Paciente.Apellidos + " Actualizado correctamente",
+                    PageRedirect = "/Pacientes/Paciente"
+                };
+
+                IsUpdatePaciente = true;
+
+            }
+
+            return Page();
+        }
+
     }
 }
