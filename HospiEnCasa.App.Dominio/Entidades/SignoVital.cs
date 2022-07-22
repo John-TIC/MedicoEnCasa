0d87b8ae124934970e87262333b09f57e37380d9
Carlos Arroyo <carloscha82@hotmail.com>
2022-07-22 13:04:25 -0500

    Se actualiza los elementos para Signos Vitales

--- HospiEnCasa.App.Dominio/Entidades/SignoVital.cs
+++ HospiEnCasa.App.Dominio/Entidades/SignoVital.cs
@@ -1,5 +1,6 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HospiEnCasa.App.Dominio
 {
@@ -8,12 +9,13 @@ namespace HospiEnCasa.App.Dominio
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Ingrese una Fecha")]
-        [DataType(DataType.Date, ErrorMessage = "Ingese una Fecha valida")]
+        [DataType(DataType.DateTime, ErrorMessage = "Ingese una Fecha valida")]
         public DateTime FechaHora { get; set; }
 
         [Required(ErrorMessage = "Ingrese un {0}")]
-        [StringLength(10, MinimumLength = 2, ErrorMessage = "{0} debe tener entre {2} y {1} caracteres")]
-        public float Valor { get; set; }
+        [DisplayFormat(DataFormatString = "{0,00}", ApplyFormatInEditMode = true)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Valor { get; set; }
         public TipoSigno Signo { get; set; }
     }
 }
