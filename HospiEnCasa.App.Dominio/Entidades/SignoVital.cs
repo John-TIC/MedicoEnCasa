11361c1fe574b118c24815f7e2fae822498b95a6
Santiago Acevedo <santiago.acevedo.m24@gmail.com>
2022-07-22 12:05:30 -0500

    validacion signo vital

--- HospiEnCasa.App.Dominio/Entidades/SignoVital.cs
+++ HospiEnCasa.App.Dominio/Entidades/SignoVital.cs
@@ -1,11 +1,18 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace HospiEnCasa.App.Dominio
 {
     public class SignoVital
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Ingrese una Fecha")]
+        [DataType(DataType.Date, ErrorMessage = "Ingese una Fecha valida")]
         public DateTime FechaHora { get; set; }
+
+        [Required(ErrorMessage = "Ingrese un {0}")]
+        [StringLength(10, MinimumLength = 2, ErrorMessage = "{0} debe tener entre {2} y {1} caracteres")]
         public float Valor { get; set; }
         public TipoSigno Signo { get; set; }
     }
